e85706189dc4a42a7f85645cc893a0e8f1b7a41a
TeslimAdeyanju <142465104+TeslimAdeyanju@users.noreply.github.com>
2025-04-18 17:27:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,17 +8,7 @@ st.info('This app builds on data collection from merchant !')
 
 with st.expander('Data'):
   st.write('**Raw data**')
-  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df = pd.read_csv('https://github.com/TeslimAdeyanju/4-Portfolio-SDS-CP023-diamond-price-predictor/blob/main/submissions-team/teslim-adeyanju/data.csv')
   df
 
-  st.write('**X**')
-  X_raw = df.drop('species', axis=1)
-  X_raw
 
-  st.write('**y**')
-  y_raw = df.species
-  y_raw
-
-
-with st.expander('🏞️ Data visualization'):
-  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
