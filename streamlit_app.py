cfd05cd2e4047c7faea28a923673106202ce4763
TeslimAdeyanju <142465104+TeslimAdeyanju@users.noreply.github.com>
2025-04-18 17:09:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title(' 💎 This is a Diamond Prediction App')
 
 st.info('This app builds on data collection from merchant !')
+
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
