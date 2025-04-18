4b351a9d3f6e734b6eaa9ae96e9cc7c25cc59687
TeslimAdeyanju <142465104+TeslimAdeyanju@users.noreply.github.com>
2025-04-18 17:37:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,7 @@ with st.expander('Data'):
 
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+# Input features
+with st.sidebar:
+  st.header('Input features')
