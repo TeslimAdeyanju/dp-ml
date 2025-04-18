88e011270a84595fa2306b9624e4594df4acee98
TeslimAdeyanju <142465104+TeslimAdeyanju@users.noreply.github.com>
2025-04-18 17:34:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,24 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
-st.title('💎 Diamond Prediction App')
+st.title('🤖 Machine Learning App')
 
-st.info('This app builds on data collection from merchant!')
+st.info('This is app builds a machine learning model!')
 
 with st.expander('Data'):
-    st.write('**Raw Data**')
-    url = 'https://raw.githubusercontent.com/TeslimAdeyanju/4-Portfolio-SDS-CP023-diamond-price-predictor/main/submissions-team/teslim-adeyanju/data.csv'
-    df = pd.read_csv(url)
-    st.dataframe(df.head())
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
 
+  st.write('**X**')
+  X_raw = df.drop('species', axis=1)
+  X_raw
+
+  st.write('**y**')
+  y_raw = df.species
+  y_raw
+
+with st.expander('Data visualization'):
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
