75830a01d1e6c8b596f442b6556a4a30964036d8
TeslimAdeyanju <142465104+TeslimAdeyanju@users.noreply.github.com>
2025-04-18 16:33:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title(' This is a Diamond Prediction App')
 
-st.write('This app builds on data collection from merchant !')
+st.info('This app builds on data collection from merchant !')
