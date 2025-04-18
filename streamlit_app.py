8e8500d387cab547fd16576363870b432f0f7acb
TeslimAdeyanju <142465104+TeslimAdeyanju@users.noreply.github.com>
2025-04-18 17:31:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
-st.title(' 💎 This is a Diamond Prediction App')
+st.title('💎 Diamond Prediction App')
 
-st.info('This app builds on data collection from merchant !')
+st.info('This app builds on data collection from merchant!')
 
 with st.expander('Data'):
-  st.write('**Raw data**')
-  df = pd.read_csv('https://github.com/TeslimAdeyanju/4-Portfolio-SDS-CP023-diamond-price-predictor/blob/main/submissions-team/teslim-adeyanju/data.csv')
-  df.head()
-
+    st.write('**Raw Data**')
+    url = 'https://raw.githubusercontent.com/TeslimAdeyanju/4-Portfolio-SDS-CP023-diamond-price-predictor/main/submissions-team/teslim-adeyanju/data.csv'
+    df = pd.read_csv(url)
+    st.dataframe(df.head())
 
